bc5d534dcac12629536ca3e5f23c4fbc1c06a52e
Francesco Minnocci <ad17fmin@uwcad.it>
2021-03-02 22:46:14 +0100

    Minor fix

--- 00/param.m
+++ 00/param.m
@@ -12,20 +12,21 @@
 	plot(X, Y, 'g','LineWidth',2)
 
 # 3
-	M=zeros(200);
-	Z=linspace(-5,5,200);
+	M=zeros(300);
+	Z=linspace(-5,5,300);
 	for i=1:200
 		for j=1:200
 			y=Z(i);
 			x=Z(j);
 			f =((x-1)^2+y^2-4)*((x+1)^2+y^2-4)*(x^2+(y-sqrt(3))^2-4);
 			if(f<10)
-				M(i,j)=1;
+				M(i,j)=10;
 			elseif(f<5)
 				M(i,j)=5;
 			elseif(f<1)
-				M(i,j)=10;
+				M(i,j)=1;
 			end
 		end
 	end
+	figure(3)
 	imagesc(M)
