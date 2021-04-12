16960d839dcf36b230ac50a988d61a88e9928614
Francesco Minnocci <ad17fmin@uwcad.it>
2021-04-12 12:14:28 +0200

    Fix rapporto_successione (division instead of difference)

--- 02/rapporto_successione.m
+++ 02/rapporto_successione.m
@@ -2,10 +2,11 @@ function r=rapporto_successione(m)
 	# Chiamo la procedure per generare i primi m termini della successione
 	g=successione(m);
 	format long
-	# Determino r (viene stampato in quanto output dello script)
-	r=g(m)-g(m-1);
+	# Determino r(rho) (come rapporto degli ultimi due elementi)
+	r=g(m)/g(m-1);
 
 	# Plot per confrontare gli andamenti di g_n e p^n
+	# Osservazione dopo la modifica: ora hanno un andamento simile)
 	for i=1:m
 		y1(i)=r^i;
 		y2(i)=g(i);
