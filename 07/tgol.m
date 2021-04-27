5d63b33ceb12778747eff19d10a6b61e73c70d0d
Francesco Minnocci <ad17fmin@uwcad.it>
2021-04-27 16:33:12 +0200

    Fix diagonal translations

--- 07/tgol.m
+++ 07/tgol.m
@@ -7,10 +7,10 @@ function tgol(n,X,k)
 		D=([zeros(1,n);X(1:n-1,:)]);
 		L=([X(:,2:n-1);zeros(n,1)]);
 		R=([zeros(n,1);X(1:n-1,:)]);
-		UL=([U(:,2:n-1);zeros(n,1)])
-		UR=([U(:,2:n-1);zeros(n,1)])
-		LD=([U(:,2:n-1);zeros(n,1)])
-		RL=([U(:,2:n-1);zeros(n,1)])
+		UL=([U(:,2:n-1);zeros(n,1)]);
+		DL=([D(:,2:n-1);zeros(n,1)]);
+		UR=([zeros(n,1);U(1:n-1,:)]);
+		DR=([zeros(n,1);D(1:n-1,:)]);
 		Y=U+D+L+R;
 		Z=X==1
 		Y+=Z
