4338274de945812127ede5d0d5b0abe595c44289
Francesco Minnocci <ad17fmin@uwcad.it>
2021-05-24 22:58:46 +0200

    More cosmetic changes

--- 10/neldermead.m
+++ 10/neldermead.m
@@ -8,10 +8,13 @@ function xmin=neldermead(f,epsilon,itermax,x1,x2,x3)
 	while((norm(x2-x1,2)+norm(x3-x1,2)>epsilon)&&(abs(f2-f1)+abs(f3-f1)>epsilon)&&(k<itermax))
 		k+=1;
 		X=[x1;x2;x3];
+
+		# Triangoli
 		line([x1(1) x2(1)],[x1(2) x2(2)],'Color','r');
 		hold on;
 		line([x2(1) x3(1)],[x2(2) x3(2)],'Color','r');
 		line([x3(1) x1(1)],[x3(2) x1(2)],'Color','r');
+
 		[F,I]=sort([f1,f2,f3]);
 		x1=X(I(1),:);
 		x2=X(I(2),:);
@@ -19,10 +22,12 @@ function xmin=neldermead(f,epsilon,itermax,x1,x2,x3)
 		f1=F(1);
 		f2=F(2);
 		f3=F(3);
+
 		xm=(x1+x2)/2;
 		xr=2*xm-x3;
 		fr=f(xr(1),xr(2));
 		fm=f(xm(1),xm(2));
+
 		if((f1<=fr)&&(fr<f2))
 			x3=xr;
 		elseif(fr<f1)
